54f56801c980e569f9464aabeb50f7ffd04678d7
ksm0814 <kksm0814@naver.com>
2018-05-24 11:55:56 +0900

    feat. Add datatable listing function and add article test datas in import.sql

--- src/main/java/overdiary/controller/HomeController.java
+++ src/main/java/overdiary/controller/HomeController.java
@@ -14,8 +14,6 @@ import javax.servlet.http.HttpSession;
 @Controller
 @RequestMapping("")
 public class HomeController {
-    // TODO : 한글 문제 해결 ㅠ 왜 다시 안되지..
-    // TODO : TEST code 추가
 
     @Resource(name = "articleService")
     ArticleService articleService;
