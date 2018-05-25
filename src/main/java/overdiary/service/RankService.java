5ccf61e799f238c585da0435c54df9f9f58d67dd
ksm0814 <kksm0814@naver.com>
2018-05-25 16:56:52 +0900

    Change. Change SimpleJob, SimpleService to RankCrawler, CrawlService and add original RankService function to new service class

--- src/main/java/overdiary/service/RankService.java
+++ src/main/java/overdiary/service/RankService.java
@@ -33,47 +33,6 @@ public class RankService {
 
     @Transactional
     public Iterable<Rank> createRankBoard() throws Exception {
-        String url = "https://overlog.gg/leaderboards";
-        Document doc = Jsoup.connect(url).get();
-
-
-        String[] ranks = doc.select("tbody").select("td").text().split("PC");
-
-        List<String> mostHeroes = new ArrayList<>();
-        for (Element img : doc.select("tbody").select("td.ContentCell-MostHeros")) {
-            mostHeroes.add(img.select("img").eachAttr("alt").toString());
-        }
-
-        List<String> rankInfo = new ArrayList<>();
-        for (String rank : ranks) {
-            String list = "";
-            for (String info : rank.split(" |Lv. |: 1 |/ |%")) {
-                if (info.equals("") || info.equals(" "))
-                    continue;
-                list += info + ",";
-            }
-            rankInfo.add(list);
-        }
-        rankInfo.remove(0);
-
-
-        Rank rank;
-        if (rankRepository.count() == 0) {
-            for (int i = 0; i < rankInfo.size(); i++) {
-                String[] info = rankInfo.get(i).split(",");
-                rank = new Rank(info[0], info[1], info[2], info[3], info[4], mostHeroes.get(i));
-                rankRepository.save(rank);
-            }
-            return rankRepository.findAll();
-        }
-
-
-        Iterable<Rank> DBRank = rankRepository.findAll();
-        for (int i = 0; i < ((List<Rank>) DBRank).size(); i++) {
-            Rank oldRank = ((List<Rank>) DBRank).get(i);
-            String[] info = rankInfo.get(i).split(",");
-            oldRank.update(info[0], info[1], info[2], info[3], info[4], mostHeroes.get(i));
-        }
         return rankRepository.findAll();
     }
 
