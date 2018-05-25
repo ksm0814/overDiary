2b53d007cadb17a699df57102cd002b6b7cc8bf8
ksm0814 <kksm0814@naver.com>
2018-05-25 15:22:20 +0900

    feat. Add quartz lib and success "hello world" task

--- src/main/java/overdiary/OverwatchDiaryApplication.java
+++ src/main/java/overdiary/OverwatchDiaryApplication.java
@@ -2,13 +2,19 @@ package overdiary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import overdiary.helper.SchedulerConfig;
+
 
 @SpringBootApplication
 @EnableJpaAuditing
+@Import({SchedulerConfig.class })
+
 public class OverwatchDiaryApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(OverwatchDiaryApplication.class, args);
 	}
 
